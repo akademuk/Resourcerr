e28147c79605d8d00fbb4cc00d8b90e0fa425339
Oleg Fedorov <akademuk24@gmail.com>
2022-06-24 11:20:36 +0300

    a

---
+++ script/phone-number.js
@@ -0,0 +1,11 @@
+var input = document.querySelector("#phone");
+intlTelInput(input, {
+  initialCountry: "auto",
+  nationalMode: false,
+  geoIpLookup: function (success, failure) {
+    $.get("https://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+      var countryCode = (resp && resp.country) ? resp.country : "us";
+      success(countryCode);
+    });
+  },
+});
