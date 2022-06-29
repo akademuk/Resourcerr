ec380a7d8b1d180688e3c9ef499a104512a34d2f
Oleg Fedorov <akademuk24@gmail.com>
2022-06-29 11:45:06 +0300

    a

---
+++ script/loader.js
@@ -0,0 +1,7 @@
+function myFunction() {
+myVar = setTimeout(showPage, 1000);
+}
+function showPage() {
+document.getElementById("loader").style.display = "none";
+document.getElementById("myDiv").style.display = "block";
+}
